7119dcf5fa533ec1fc429b068d3c17e703f0f769
Justin Stout <jstout38@gmail.com>
2015-09-27 21:50:14 -0400

    ready to submit

---
+++ gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+            src: [
+                'js/libs/*.js', // All JS in the libs folder
+                'js/*.js'
+            ],
+            dest: 'js/build/production.js',
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'img/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'img/build/'
+                }]
+            },
+            pizza: {
+                files: [{
+                    expand: true,
+                    cwd: 'views/images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'views/images/build/'
+                }]
+            }
+        },
+
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
+
+};
--- views/js/main.js
+++ views/js/main.js
@@ -570,11 +570,11 @@ function updatePositions() {
 window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
-// Optimized by reducing from 200 to 35
+// Optimized by reducing from 200 to 32
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 35; i++) {
+  for (var i = 0; i < 32; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
